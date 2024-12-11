ebf8cda313a0a8d5e8b796df27ea5f30bf57eac5
Kevin <Kevin.Kuck@gmx.de>
2024-12-11 13:10:18 +0100

    recap erste Sitzung

--- main.py
+++ main.py
@@ -1,1 +1,27 @@
 # Variablen
+import pandas as pd
+
+
+
+a = 2
+b = "hello "
+c = 'world'
+d= "4"
+# Debugging
+
+# string manipulation
+print(b+c)
+
+
+print(f"hello {c}")
+
+name = input("wie lautet dein Name")
+
+print(f"hello {name}")
+
+
+# Erstellt einen Namensgenerator für euer Projekt.
+# Es sollen Variablen genutzt werden, und User input.
+# Im Fehlerfall bitte eure "Debugging"-Kenntnisse nutzen und die Fehler identifizieren.
+
+print(a+str(d))
