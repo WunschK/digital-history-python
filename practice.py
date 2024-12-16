4123e188053c31d396539ffb76ded67f16c8f67a
KWunsch <kevin.wunsch@tu-darmstadt.de>
2024-12-16 04:22:28 +0100

    added practice.py

---
+++ practice.py
@@ -0,0 +1,51 @@
+#1 ) Gib alle Zahlen von 1 - 100 aus
+
+n = 0
+while(n < 100):
+    n = n + 1
+    print(n)
+
+
+
+# 2) Ersetze alle Zahlen die durch drei teilbar sind durch "digital"
+n = 0
+while(n < 100):
+    n = n + 1
+    if(n % 3 == 0):
+        print("digital")
+    else:
+        print(n)
+
+# 3) Ersetze alle Zahlen die durch fünf teilbar sind durch "history"
+
+n = 0
+while(n < 100):
+    n = n + 1
+    if(n % 5 == 0):
+        print("history")
+    else:
+        print(n)
+
+# 4) Kombiniere die beiden obigen Schritte mit einem "elif"
+
+n = 0
+while(n < 100):
+    n = n + 1
+    if(n % 3 == 0):
+        print("digital")
+    elif(n % 5 == 0):
+        print("history")
+    else:
+        print(n)
+
+
+# 5) Ersetze alle Zahlen die durch drei und fünf teilbar sind durch "digital history"
+
+n = 0
+while(n < 100):
+    n = n + 1
+    if(n % 3 == 0 and n % 5 == 0):
+        print("digital history")
+    else:
+        print(n)
+
